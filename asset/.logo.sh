f7d76a227c63ef58dfc2e1f6dc7e4a998804f0ec
baphomate.inc@gmail.com <baphomate.inc@gmail.com>
2022-09-25 00:54:47 +0700

    menu home

---
+++ asset/.logo.sh
@@ -0,0 +1,34 @@
+clear
+echo -e """\e[1;31m
+                                                                      
+                                                                      
+               ~^:...                             ...^^               
+               :~!!777!!7!!~^.    ~7.   :^~!!777!777!~.               
+                  :^^~7?7^~!JJ~~::J?::~~J?!~::...                     
+                :~~^~~~^^    ~Y?:~?!^^YJ^                             
+               :77!^     .^~7?JG?!J?~5PJJ?!^.                         
+               ~7G~   .^!?YJ5J?!!Y?JJ~7J5555J?~.                      
+             ^~.75. .!JJY?7?YYY!~^!~:!?GGG5P!!5J!.                    
+             ~?~.7~!57J~7??G#&G~~^^^~^?&&&YY5?77#Y^                   
+            :!7:^ ?Y7??555?!B#5?7~.^7?7B#5!Y5YY?7#B7.                 
+             ~!.~77^7J#5YPYYJ5PYJ^.:!JY5J7Y5GJJJ~!G#J.                
+              7^^P!!&GPY?5G?..:?J?77JY~ ..~YYYPY5!~&G!                
+              .~.:!!#G5P~~:    :Y7J?Y?   ^: .J55YJ^GG?.               
+               ^5~ ^B?~   :5?:  ^77?7. .!BY  .JPJ5~!BY^               
+               :5P5^~^^JYYYJY!~~^:?^~!^^!PP7: .YYJ:!GJ:               
+\e[1;37m                ??YBYPPP5?P5G^ .:^J!: :7?B?~5~ .JJ!?#7.               
+                ^J!PGBP?Y5#&&?:!::!~~~7P557!7Y~  ^YG5~                
+                 !?5?Y?5#&@&@P?Y7JYYYJJJ55J~7GBY! .J7                 
+                  !5PB^^G@B5YJ775BP5?!7J5PJ?7&5!BP~.~.                
+                   :?&G?YY?Y7J:^Y#YJYJ77~~~!!~:YP?Y?:::^^.            
+                     ^?P#B?!JGPY5Y!JGPJ!7!~!5YJ?::7?~^!!~.
+                     .~?5P55BJ5J??!P5JJ!JGY!~^^~~^
+                       .:!!JJYJ???JJ?!~:^~!??!^
+"""
+baphomate="                     "
+echo -e "$baphomate\e[31m•\e[33m•\e[32m•\e[37m[\e[32m#\e[37m]\e[1;36m Baphomate Inc \e[37m[\e[32m#\e[37m]\e[32m•\e[33m•\e[31m•"
+kenichie=echo
+$kenichie -e "\e[37m______________________________________________________________________"
+$kenichie -e " $baphomate\e[37m[\e[32m#\e[37m]\e[36m Welcome To Toolkit \e[37m[\e[32m#\e[37m]"
+$kenichie -e " $baphonate\e[33m                            Version: \e[31m1.0.1"
+$kenichie -e "\e[37m______________________________________________________________________"
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 """
    Created by Kenichie from the Baphomate Inc . team
    ___________________________________________________
+   Bersamaku open source :)
    Gausah recode mek :v 
    Jadi lah programer yang non recode,hargai program orang lain
    tanpa harus merecode program orang lain.
@@ -10,3 +11,46 @@
    pelajari lalu bikin sendiri :)"
    
 """
+import os,sys,time,random
+def main():
+    #**//warna text seperti warna hati ku warna warni :v
+    class cl:
+        red="\033[1;31m"
+        grn="\033[1;32m"
+        ylw="\033[1;33m"
+        blu="\033[1;34m"
+        pop="\033[1;35m"
+        cyn="\033[1;36m"
+        wht="\033[1;37m"
+        dep="\033[0m"
+        #*//done✓
+        
+    #*****///Batas suci cintaku pada mu uwuwuwuw :v
+    def clear():
+        os.system('clear')
+        #*//done✓
+        
+    #***//batas pemisah antara aku dan kamu >_<
+    def banner():
+        os.system('bash asset/.logo.sh')
+        #*//done✓
+    #***//home menu 
+    def home():
+        banner()
+        print("┌─[MENU]─[Baphomate.Inc]
+        print("│")
+        print("│")
+        print("├──• 1.Install Packagess(recommend)")
+        print("├──• 2.Start")
+        print("├──• 3.Update")
+        print("├──• 4.Join the Grup")
+        print("├──• 5.Report bug")
+        print("├──• 0.Exit program")
+        print("│")
+        print("│")
+        #*//done✓
+    #***//udud heula atu
+    home()
+    pil=int(input("└──╼ # "))
+       
+main()    
