181121c37c60a549e1043ce26bd97ec0c789b845
baphomate.inc@gmail.com <baphomate.inc@gmail.com>
2022-09-24 22:45:36 +0700

    python main

---
+++ main.py
@@ -0,0 +1,12 @@
+#***//utf-8 android 8 (oreo) | !/bin/python version 3.10.7
+"""
+   Created by Kenichie from the Baphomate Inc . team
+   ___________________________________________________
+   Gausah recode mek :v 
+   Jadi lah programer yang non recode,hargai program orang lain
+   tanpa harus merecode program orang lain.
+   cari referensi boleh recode jangan:v.
+   "Recode tidak akan menjadikan anda mastah maupun suhu
+   pelajari lalu bikin sendiri :)"
+   
+"""
